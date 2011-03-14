605c18291d00bafe223f10691d63d24b5adab312
doaitse <doaitse@b22e4a66-0d8d-4a73-aa21-d6b4dd4dcce4>
2011-03-14 20:26:02 +0000

    Refined the definitions in the class idioms a bit.
    git-svn-id: https://svn.science.uu.nl/repos/project.STEC.uu-parsinglib/trunk@97 b22e4a66-0d8d-4a73-aa21-d6b4dd4dcce4

--- src/Text/ParserCombinators/UU/Idioms.hs
+++ src/Text/ParserCombinators/UU/Idioms.hs
@@ -6,6 +6,8 @@
              FlexibleContexts,
              CPP #-}
 
+#define DEMO(p,i) demo "p" i p
+
 module Text.ParserCombinators.UU.Idioms where
 
 import Text.ParserCombinators.UU
@@ -15,41 +17,41 @@ import Text.ParserCombinators.UU.Demo.Examples hiding (show_demos)
 import qualified Data.ListLike as LL
 import Control.Applicative 
 
+data String' = String' {fromStr :: String}
 
 -- | The  `Ii` is to be pronounced as @stop@
 data Ii = Ii 
 
 -- | The function `iI` is to be pronounced as @start@
-iI :: Idiomatic  (Str Char state loc) (a -> a) g => g
+iI ::Idiomatic  i (a -> a) g => g
 iI = idiomatic (pure id)
+
 
 class Idiomatic st f g  | g -> f st  where
     idiomatic :: P st f -> g
-instance  Idiomatic (Str Char state loc) x  (Ii -> P  (Str Char state loc) x) where
+instance  Idiomatic st x  (Ii -> P st x) where
     idiomatic ix Ii = ix
-instance (Idiomatic  (Str Char state loc) f g, IsLocationUpdatedBy loc Char, LL.ListLike state Char) 
-       => Idiomatic  (Str Char state loc) (a -> f) (P  (Str Char state loc) a -> g) where
+instance  Idiomatic st f g  => Idiomatic  st (a -> f) (P  st a -> g) where
     idiomatic isf is = idiomatic (isf <*> is)
+instance Idiomatic st f g => Idiomatic st ((a -> b) -> f)  ((a -> b) -> g) where
+    idiomatic isf f = idiomatic (isf <*> (pure f))
+
 instance (Idiomatic  (Str Char state loc) f g, IsLocationUpdatedBy loc Char, LL.ListLike state Char) 
        => Idiomatic  (Str Char state loc) f (String -> g) where
     idiomatic isf str = idiomatic (isf <* pToken str)
 instance  (Idiomatic (Str Char state loc) f g, IsLocationUpdatedBy loc Char, LL.ListLike state Char) 
       =>   Idiomatic (Str Char state loc) f (Char -> g) where
     idiomatic isf c = idiomatic (isf <* pSym c)
-instance Idiomatic st f g => Idiomatic st ((a -> b) -> f)  ((a -> b) -> g) where
-    idiomatic isf f = idiomatic (isf <*> (pure f))
+
 
 -- | The idea of the Idiom concept is that  sequential composition operators can be inferred from the type 
 --   of the various operands
 --
--- >>> run (iI (+) '(' pNatural "+"  pNatural ')' Ii) "(2+3"
+-- >>> run (iI (+) '(' pNatural "plus"  pNatural ')' Ii) "(2 plus 3"
 --   Result: 5
 --    Correcting steps: 
 --      Inserted  ')' at position LineColPos 0 4 4 expecting one of [')', Whitespace, '0'..'9']
 --
-test :: Parser Int
-test = iI (+) '(' pNatural "+" pNatural ')' Ii
 
-#define DEMO(p,i) demo "p" i p
 
-show_demos =  demo "(iI (+) '(' pNatural \"+\" pNatural ')' Ii)::Parser Int" "(2+3)" ((iI (+) '(' pNatural "+" pNatural ')' Ii)::Parser Int)
+show_demos =  demo "(iI (+) '(' pNatural \"plus\" pNatural ')' Ii)::Parser Int" "(2 plus 3)" ((iI (+) '(' pNatural "+" pNatural ')' Ii)::Parser Int)
